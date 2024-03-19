16815745716210fb809491626ec9b51d96239b31
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-19 08:37:23 +0300

    create an instance of User class

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        User user1 = new User();
+        user1.name = "Caleb";
+        user1.membership = "Gold";
+
+
+        User user2 = new User();
+        user2.name = "Sally";
+        user2.membership = "Silver";
+
+
+        System.out.println(user1.name);
+        System.out.println(user2.name);
+        System.out.println(user1.membership);
+        System.out.println(user2.membership);
+        System.out.println(user1.age);
+
+
     }
 }
